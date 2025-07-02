04d0d1953cc2db6672dc81f8861d1184cbcd27c7
makenziemgray <makenziemgray0102@gmail.com>
2025-07-02 11:00:54 -0400

    initiated client file for connect function

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // event listener for incoming messages from server
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
